435d9087a0e084fc290a74a5cc7d358eb615655c
LuddeW <ludwig.wiktorsson@gmail.com>
2020-02-01 21:50:27 +0100

    added hashtags and another source

--- bin/update.rb
+++ bin/update.rb
@@ -11,21 +11,35 @@ end
 
 latest_tweets = twitter.user_timeline("LFC_NewStories")
 
+
 previous_links = latest_tweets.map do |tweet|
     if tweet.urls.any?
         tweet.urls[0].expanded_url
     end
 end
 
-rss = HTTParty.get("https://www.liverpoolfc.com/news.rss")
-doc = Nokogiri::XML(rss)
+lfc_com = HTTParty.get("https://www.liverpoolfc.com/news.rss")
+doc = Nokogiri::XML(lfc_com)
 
-doc.css("item").take(5).each do |item|
+doc.css("item").take(2).each do |item|
     title = item.css("title").text
 
     link = item.css("link").text
 
     unless previous_links.include?(link)
-        twitter.update("#{title} #{link}")
+        twitter.update("#{title} #{link} #LFC #FPL")
+    end
+end
+
+lfcmedia_com = HTTParty.get("https://www.liverpoolfc.com/news/media-watch.rss")
+docOne = Nokogiri::XML(lfcmedia_com)
+
+docOne.css("item").take(2).each do |item|
+    title = item.css("title").text
+
+    link = item.css("link").text
+
+    unless previous_links.include?(link)
+        twitter.update("#{title} #{link} #LFC #FPL")
     end
 end
